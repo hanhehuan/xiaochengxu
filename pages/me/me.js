fe5c699367c373434972a0dc2e596e07772c69d2
hanhehuan <1242017454@qq.com>
2019-05-13 17:43:35 +0800

    first

---
+++ pages/me/me.js
@@ -0,0 +1,78 @@
+// pages/me/me.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg:"nihao",
+    num:100,
+    person:{
+      name:"小红",
+      age:60
+    },
+    isChecked:true,
+    
+    num1:10,
+    num2:20,
+    str1:"1111",
+    str2:"2222",
+    arr:['香蕉','苹果','香梨']
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
